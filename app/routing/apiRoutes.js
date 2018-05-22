48174a5286b84b6d5ffb2e18c5ef36f9064110ba
dvdkrstndnls <karstenisgerman@yahoo.com>
2018-05-22 18:33:10 -0500

    pseudocoded some logic

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,4 +1,4 @@
-var characters = require("../data/friend.js")
+var characters = require("../data/friends.js")
 
 
 
@@ -10,5 +10,20 @@ module.exports = function(app) {
   
   app.post("/survey", function(req, res) {
     res.sendFile(path.join(__dirname, "../public/survey.html"));
+  //outer for loop with [i]  loop over each existing characters function ingest characters
+      
+   
+        // (function(array1, array2) caluculate difference ie compare scores "find distance between sum of 2 arrays" differnce) inner for loop using [j] loop over new character scors and compare to [i]
+              //compare to me and add the 
+              // add up the difference for each score (math.abs)
+
+//var persist the closest match ie least difference 
   });
   }
+
+  app.get("/api/friends", function(req, res) {
+    res.JSON(characters)
+  });
+  
+  app.post("/api/friends", function(req, res) {
+    res.JSON(comparisonResult));
